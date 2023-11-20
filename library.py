5a5d4e8f359fe4c47f65aa43f42e7facb2d2b14d
piyush <piyush.khadka2233@gmail.com>
2023-11-20 21:11:59 +0000

    a base for buying a book

--- library.py
+++ library.py
@@ -16,7 +16,7 @@ class LMS:
             print('b')
             
     def readfile(self):
-        with open ('storage.txt', 'r') as f:
+        with open ('data.txt', 'r') as f:
           for line in f:
               print(line.strip())
         f.close()
@@ -27,17 +27,27 @@ class LMS:
         author=str(input('Enter the author of the book:'))
         stock=int(input('Enter the stock of the book:'))
         price=int(input('Enter the price of the book:'))
-        with open ('storage.txt','a') as f:
+        with open ('data.txt','a') as f:
             f.write(f'{title},{author},{stock},{price}')
             f.close
 
     def buyabook(self):
-      print("Enter the details of the book you want to buy")
-      #title=str(input('Enter the title of the book:'))
-      with open ('storage.txt','r') as f:
-          line=f.readlines
-          for i in line:
-              print(i)
+        print("Enter the details of the book you want to buy")
+        btitle=str(input('Enter the title of the book:'))
+        bamount=int(input('Enter the amount of books you want to buy limited to 3 person:'))
+        with open ('data.txt','r+') as f:
+            lines=f.readlines()
+
+            for line in lines:
+                bdetails= line.strip().split(',')
+
+                if btitle==bdetails[0]:
+                    bdetails[2]=str(int(bdetails[2])-bamount)
+                    f.seek(0)
+                    line = ','.join(bdetails) + '\n'
+
+                f.write(line)
+
     def borrowbooks(self):
         pass
 
