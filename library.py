e8db418112cd92daea99cd92beed266d69d959ed
piyush <piyush.khadka2233@gmail.com>
2023-11-21 18:27:32 +0000

    Added exit and continue functions, buy a book still needs a little fix

--- library.py
+++ library.py
@@ -7,89 +7,104 @@ class LMS:
             view = int(input('Enter your option from 1 to 4:'))
         
             if view == 1:
-                return self.readfile()
+                self.readfile()
 
             elif view == 2:
-                return self.addabook()
+                self.addabook()
 
             elif view == 3:
-                return self.buyabook()
+                self.buyabook()
         
             elif view == 4:
-                sure=str(input('Are you sure you want to exit? y or n.'))
-                if sure == 'y':
-                    print('Bye!')
-
-                elif sure == 'n':
-                    print(book.options())
-
-                else:
-                    print(f'Enter a valid value. {book.options()}')
+                self.exit_program()
         
             else:
                 print('Please enter a valid number!')
-                print(book.options())
+                self.options()
                 
-
         except ValueError as e:
             print(f'Invalid input! {e}')
             print(book.options())
             
     def readfile(self):
         with open ('data.txt', 'r') as f:
+          print('Here are the list of books in the library.')
           for line in f:
               print(line.strip())
-        f.close()
+          self.continue_program()
             
     def addabook(self):
         print('Add the required info for the book to be added in the library.')
         try:
-            title=str(input('Enter the title of the book:')).title()
-            author=str(input('Enter the author of the book:'))
-            stock=int(input('Enter the stock of the book:'))
-            price=int(input('Enter the price of the book:'))
+            title = str(input('Enter the title of the book:')).title()
+            author = str(input('Enter the author of the book:'))
+            stock = int(input('Enter the stock of the book:'))
+            price = int(input('Enter the price of the book:'))
             with open ('data.txt','a') as f:
                 f.write(f'\n{title},{author},{stock},${price}')
-                f.close
+                f.close()
             print('The book has been added to the library.') 
-            con=str(input('Do you wish to continue? type \'y\' for yes and \'n\' for no.')).capitalize()
-            if con == 'Y':
-                print(book.options())
-                
-            elif con == 'N':
-                print('Thank you for your time.')
-
-            else: 
-                print('Invalid input, try again.')
-                print(book.options())
+            self.continue_program()
 
         except Exception as e:
             print(f'Invalid input. {e}')
             print(book.options())
 
     def buyabook(self):
-        print("Enter the details of the book you want to buy")
+        print("Enter the details of the book you want to buy.")
+        
         try :
     
-            btitle=str(input('Enter the title of the book:')).title()
-            bamount=int(input('Enter the amount of books you want to buy limited to 3 person:'))
+            btitle = str(input('Enter the title of the book:')).title()
+            book_found = False
             with open ('data.txt', 'r') as f:
-                lines=f.readlines()
+                lines = f.readlines()
+            updated_lines = []
 
+            for line in lines:
+                bdetails = line.strip().split(',')
+
+                if btitle == bdetails[0]:
+                    book_found = False
+                    bamount = int(input('Enter the amount of books you want to buy limited to 3 person:'))
+                    current_stock=int(bdetails[2])
+                    if 0 < bamount <=3 and current_stock >= bamount:
+                        bdetails[2] = str(current_stock - bamount)
+                        updated_lines.append(','.join(bdetails) + '\n')
+                    else :
+                        print('Invalid input! Either the amount is invalid or out of stock.')
+                else:
+                        updated_lines.append(lines)
             with open ('data.txt', 'w') as f:
-                for line in lines:
-                    bdetails= line.strip().split(',')
+                f.writelines(updated_lines)
+                            
+            if not book_found:
+                    print('There is no such book in the database.')
 
-                    if btitle==bdetails[0]:
-                        bdetails[2]=str(int(bdetails[2]) - bamount)
-                        f.write(','.join(bdetails)+'\n')
-                        
-                    else:
-                        f.write(line)      
+            self.continue_program()
                 
         except Exception as e:
             print(f'Enter a Valid Input!{e}')
+            self.options()
 
+    def exit_program(self):
+        sure = str(input('Are you sure you want to exit? y or n.')).capitalize()
+        if sure == 'Y':
+                print('Bye!')
+        elif sure == 'N':
+            print(book.options())
+        else:
+            print(f'Enter a valid value. {self.options()}')
+
+    def continue_program(self):
+        con = str(input('Do you wish to continue? Type \'y\' for yes and \'n\' for no.')).capitalize()
+        if con == 'Y':
+            self.options()
+        elif con == 'N':
+            print('Thank you for your time.')
+        else:
+            print('Invalid input, try again.')
+            self.options()
 
 book=LMS()
 book.options()
